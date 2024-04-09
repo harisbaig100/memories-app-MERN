31fa62d4e69251e08b0aa284f7864921d4e0502a
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-04-09 16:53:00 +0500

    background image

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import {thunk} from'redux-thunk';
 
+import './index.css'
 import reducers from './reducers';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
